f90ac978991fed693589d751db8df53c59bb150d
duncanista <30836115+duncanista@users.noreply.github.com>
2019-09-01 14:27:12 -0500

    Update code

--- contact.php
+++ contact.php
@@ -8,8 +8,8 @@ include "templates/header.php";
 	<div class="container">
 		<div class="row align-items-center">
 			<div class="col-md-10 margin-top-1">
-				<span class="d-block mb-3 caption" data-aos="fade-up">Get In Touch</span>
-				<h1 class="d-block mb-4" data-aos="fade-up" data-aos-delay="100">Contact Us</h1>
+				<span class="d-block mb-3 caption" data-aos="fade-up">Queremos escucharte</span>
+				<h1 class="d-block mb-4" data-aos="fade-up" data-aos-delay="100">Contáctanos</h1>
 			</div>
 		</div>
 	</div>
@@ -24,11 +24,11 @@ include "templates/header.php";
 
 					<div class="row form-group">
 						<div class="col-md-6 mb-3 mb-md-0">
-							<label class="" for="fname">First Name</label>
+							<label class="" for="fname">Nombre</label>
 							<input type="text" id="fname" class="form-control">
 						</div>
 						<div class="col-md-6">
-							<label class="" for="lname">Last Name</label>
+							<label class="" for="lname">Apellido</label>
 							<input type="text" id="lname" class="form-control">
 						</div>
 					</div>
@@ -44,21 +44,21 @@ include "templates/header.php";
 					<div class="row form-group">
 
 						<div class="col-md-12">
-							<label class="" for="subject">Subject</label>
+							<label class="" for="subject">Asunto</label>
 							<input type="subject" id="subject" class="form-control">
 						</div>
 					</div>
 
 					<div class="row form-group">
 						<div class="col-md-12">
-							<label class="" for="message">Message</label>
+							<label class="" for="message">Mensaje</label>
 							<textarea name="message" id="message" cols="30" rows="7" class="form-control"></textarea>
 						</div>
 					</div>
 
 					<div class="row form-group">
 						<div class="col-md-12">
-							<input type="submit" value="Send Message" class="btn btn-primary py-2 px-4 text-white">
+							<input type="submit" value="Enviar Mensaje" class="btn btn-primary py-2 px-4 text-white">
 						</div>
 					</div>
 
@@ -67,14 +67,17 @@ include "templates/header.php";
 			</div>
 			<div class="col-md-5 ml-auto" data-aos="fade-up" data-aos-delay="100">
 				<div class="p-4 mb-3">
-					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Address</p>
-					<p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
+					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Tec de Monterrey, CEM</p>
+					<p class="mb-4">Av Lago de Guadalupe KM 3.5, Margarita Maza de Juárez, 52926 Cd López Mateos, Méx.</p>
 
-					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Phone</p>
-					<p class="mb-4"><a href="#">+1 232 3235 324</a></p>
+					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Tec de Monterrey, CCM</p>
+					<p class="mb-4">Calle Puente 222, Coapa, Arboledas del Sur, 14380 Ciudad de México, CDMX</p>
 
-					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Email Address</p>
-					<p class="mb-0"><a href="#">youremail@domain.com</a></p>
+					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Tec de Monterrey, CSF</p>
+					<p class="mb-4">Av Carlos Lazo 100, Santa Fe, La Loma, 01389 Ciudad de México, CDMX</p>
+
+					<p class="mb-0 font-weight-bold text-secondary text-uppercase mb-3">Correo de CC19</p>
+					<p class="mb-0"><a href="#">info@codechallenge.mx</a></p>
 
 				</div>
 			</div>
